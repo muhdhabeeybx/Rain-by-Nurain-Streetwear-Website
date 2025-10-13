318d7c47e1f5e7f09b4609e4316ccb7768fa3bdd
muhdhabeeybx <122448219+muhdhabeeybx@users.noreply.github.com>
2025-10-13 09:01:29 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from './ui/button';
 import { motion } from 'framer-motion';
-const heroImage = 'src/assets/rbnhero.png';
+const heroImage = '/assets/rbnhero.png';
 
 export function HeroSection() {
   const scrollToNewArrivals = () => {
