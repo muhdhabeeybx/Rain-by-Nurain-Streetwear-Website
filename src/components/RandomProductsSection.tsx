07714ff1b45352bd159b4a2b10e0c2c6a60390d4
muhdhabeeybx <122448219+muhdhabeeybx@users.noreply.github.com>
2025-10-15 12:51:30 +0100

    Update RandomProductsSection.tsx

--- src/components/RandomProductsSection.tsx
+++ src/components/RandomProductsSection.tsx
@@ -77,10 +77,10 @@ export function RandomProductsSection() {
         <motion.div
           className="
             grid 
-            grid-cols-2 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4 
+            grid-cols-1 
+            sm:grid-cols-1 
+            md:grid-cols-2 
+            lg:grid-cols-3 
             gap-6 
             sm:gap-8
           "
